4626703489f79234a8b07da0e13b4e6d34fd4f8b
Martin Lenart <martin_lenart@icloud.com>
2022-03-16 17:19:52 +0100

    Add project files.

---
+++ SeidoDemoDb/SeidoDemoDbContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+using SeidoDemoModels;
+
+namespace SeidoDemoDb
+{
+    public class SeidoDemoDbContext:DbContext
+    {
+        #region Uncomment to create the Data model
+        //public DbSet<Customer> Customers { get; set; }
+        //public DbSet<Order> Orders { get; set; }
+        #endregion
+
+        public SeidoDemoDbContext() { }
+        public SeidoDemoDbContext(DbContextOptions options) : base(options)
+        { }
+ 
+        #region Uncomment to create the Data model
+        /*
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var connectionString = DBConnection.ConfigurationRoot.GetConnectionString("MariaDB_seidodemo");
+                optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+        */
+        #endregion
+    }
+}
